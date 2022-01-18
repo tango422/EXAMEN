aaf23ffdde34e387b46a64b45ec94d44c962c6b2
unknown <paguilar3330@gmail.com>
2022-01-17 21:48:15 -0300

    first commit

---
+++ src/test/java/com/petSmile/NTest.java
@@ -0,0 +1,45 @@
+package com.petSmile;
+
+import java.util.List;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.testng.annotations.Test;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import static org.junit.Assert.assertEquals;
+
+
+
+
+@SuppressWarnings("unused")
+
+public class NTest {
+	public NTest() {
+		
+	}
+	
+	@BeforeClass
+	public static void setUpClass() {
+		
+	}
+	@AfterClass
+	public static void tearDownClass() {
+		
+	}
+	
+	@BeforeTest
+	public void setUp(){
+		
+	}
+	
+	@AfterTest
+	public void tearDown(){
+		
+	}
+	
+  @Test
+  public void f() {
+  }
+}
---
+++ src/test/java/conexion/Conexion.java
@@ -0,0 +1,38 @@
+
+package conexion;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexion {
+
+	private static final String CONTROLADOR = "com.mysql.jdbc.Driver";
+	private static final String URL = "jdbc:mysql://localhost:3306/clinica";
+	private static final String USUARIO = "root";
+	private static final String CLAVE = "root";
+
+	public Connection conectar() {
+		Connection conexion = null;
+		
+		try {
+			Class.forName(CONTROLADOR);
+			conexion = DriverManager.getConnection(URL, USUARIO, CLAVE);
+			System.out.println("Conexión OK");
+
+		} catch (ClassNotFoundException e) {
+			System.out.println("Error al cargar el controlador");
+			e.printStackTrace();
+
+		} catch (SQLException e) {
+			System.out.println("Error en la conexión");
+			e.printStackTrace();
+		}
+		
+		return conexion;
+	}
+
+	public static void main(String[] args) {
+
+	}
+}
